242c6ab287decae125eba810a98134a5bc35026e
Leonard Gonzalez <lions518@gmail.com>
2016-05-09 12:59:03 -0430

    Manejo de rutas por anotacion

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -2,23 +2,30 @@
 
 namespace Cupon\OfertaBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/sitio/{pagina}/", name="pagina_estatica")
+     */
     public function ayudaAction()
     {
         return $this->render('OfertaBundle:Default:ayuda.html.twig');
     }
     
+    /**
+     * @Route("/", name="portada")
+     */
     public function portadaAction()
     {
         $em = $this->getDoctrine()->getEntityManager();
         
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
             'ciudad'            => 1,
-//            'fecha_publicacion' => new \DateTime('today')
+            // 'fecha_publicacion' => new \DateTime('today')
         ));
 
         return $this->render(
