cfc8d39175948c48a66d861de91150ba6e7c1010
Leonard Gonzalez <lions518@gmail.com>
2016-05-16 00:18:00 -0400

    Gestion de errores p 124

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Cupon\OfertaBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -17,16 +18,28 @@ class DefaultController extends Controller
     }
     
     /**
-     * @Route("/", name="portada")
+     * @Route("/{ciudad}", defaults={"ciudad" = 1}, name="portada", )
      */
-    public function portadaAction()
+    public function portadaAction($ciudad = null)
     {
+        if ($ciudad == null){
+            $ciudad = $this->container
+                            ->getParameter('cupon.ciudad_por_defecto');
+
+            return new RedirectResponse(
+                $this->generateUrl('portada', array('ciudad' => $ciudad))
+            );
+        }
         $em = $this->getDoctrine()->getManager();
         
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
-            'ciudad'            => 1,
+            'ciudad'            => $this->container->getParameter('cupon.ciudad_por_defecto'),
             // 'fecha_publicacion' => new \DateTime('today')
         ));
+
+        if (!$oferta){
+            throw $this->createNotFoundException('No se ha encontrado la oferta del día en la ciudad seleccionada');
+        }
 
         return $this->render(
             'OfertaBundle:Default:portada.html.twig',
