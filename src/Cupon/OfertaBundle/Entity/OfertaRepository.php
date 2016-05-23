c451870bf81084fe4b8f3693f5c5da6b6047fc9c
Leonard Gonzalez <lions518@gmail.com>
2016-05-23 16:03:12 -0430

    Query dql pg130

--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -7,6 +7,21 @@ class OfertaRepository extends EntityRepository
 {
     public function findOfertaDelDia($ciudad)
     {
-        
+        $em = $this->getEntity();
+        $dql = 'SELECT o, c, t
+                    FROM OfertaBundle:Oferta o
+                    JOIN o.ciudad c JOIN o.tienda
+                    WHERE o.revisada = true
+                    -- AND o.fecha_publicacion < :fecha
+                    AND c.slug = :ciudad
+                    ORDER BY o.fecha_publicacion DESC';
+
+        $consulta = $em->createQuery($dql);
+//        $consulta->setParameter('fecha', new \DateTime('now'));
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setMaxResults(1);
+
+        return $consulta->SigleResult();
+
     }
 }
